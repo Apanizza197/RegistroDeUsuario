9e0ea31226b3d28d8e9c9d43f248618de4d53b26
mariagracianin <mariagracia.nin@gmail.com>
2022-08-28 10:23:30 -0300

    ok

--- registroUsuario/src/main/java/com/example/registroUsuario/UsuarioController.java
+++ registroUsuario/src/main/java/com/example/registroUsuario/UsuarioController.java
@@ -1,8 +1,14 @@
 package com.example.registroUsuario;
 
+import org.hibernate.annotations.GeneratorType;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UsuarioController {
     //funcion para guardar datos
+
+    public void saveUser(){
+
+
+    }
 }
---
+++ registroUsuario/src/main/java/com/example/registroUsuario/UsuarioService.java
@@ -0,0 +1,7 @@
+package com.example.registroUsuario;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+}
